714522b5ac81416503f56cbbffa5d9f3d8fd2302
ywais <yuvalwais@gmail.com>
2020-08-03 13:11:08 +0300

    initialization

---
+++ main.js
@@ -0,0 +1,5 @@
+const searchPokemon = async (pokemonId = 3) => {
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    console.log(data)
+  };
+  searchPokemon();  
