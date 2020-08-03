c0a4656385146cbe3ea5d3f334aa02cb147d044b
ywais <yuvalwais@gmail.com>
2020-08-03 18:15:33 +0300

    first task
    Show Pockemon name, height, weight and image on search id

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const searchPokemon = async (pokemonId = 3) => {
+const searchInput = document.querySelector('#search'),
+      searchButton = document.querySelector('#searchButton'),
+      resultsDiv = document.querySelector('#results');
+
+const searchPokemon = async (pokemonId) => {
     const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    console.log(data)
-  };
-  searchPokemon();  
+    addFoundPokemon(data);
+};    
+
+searchButton.addEventListener('click', () => {searchPokemon(searchInput.value)});
+
+function addFoundPokemon(pokeData) {
+    const htmlText = `
+      <div class="pokemonContainer">
+        <div>Name: ${pokeData.name}</div>
+        <div>height: ${pokeData.height}</div>
+        <div>weight: ${pokeData.weight}</div>
+        <div>picture: <img src="${pokeData.sprites.front_default}" /></div>
+      </div>
+    `;
+    resultsDiv.innerHTML = htmlText;
+}
